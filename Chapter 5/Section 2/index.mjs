a584703f05aa6730ff9739c0972270af45d0895f
awsbookresource <liguo.yu@gmail.com>
2023-06-08 13:43:53 -0400

    Update index.mjs

--- Chapter 5/Section 2/index.mjs
+++ Chapter 5/Section 2/index.mjs
@@ -1,1 +1,27 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
 
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+    body = await dynamo.send(
+    new ScanCommand({ TableName: 'Events' })
+    );
+    body = body.Items;
+  
+  return {
+  statusCode,
+  body,
+  headers,
+  };
+};
